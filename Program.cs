a0fe12cbebcf38e60e388064d416c75776524269
Vanesa <57409926+vanesa33@users.noreply.github.com>
2021-02-09 14:39:38 -0300

    Práctica en consola C sharp para principiantes
    proyecto utilizando Try Catch

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+
+namespace tryCatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double precio, total;
+            int cantidad;
+
+            try
+            {
+                Console.WriteLine("cantidad: ");
+                cantidad = Convert.ToInt16(Console.ReadLine());
+
+                Console.WriteLine("Precio: ");
+                precio = Convert.ToDouble(Console.ReadLine());
+
+                total = precio * cantidad;
+
+                Console.WriteLine("el total es: " + total);
+            }
+
+             catch(Exception error)
+            {
+
+                Console.WriteLine("ha ocurrido un error" + error.Message);
+            }
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
